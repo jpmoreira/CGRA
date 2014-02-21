b27937c91b522d8006d1a55a7585fc5218ac6860
Jose Moreira <ei12002@fe.up.pt>
2014-02-21 21:39:29 +0000

    Added Triangle

--- ExampleObject.cpp
+++ ExampleObject.cpp
@@ -2,6 +2,12 @@
 
 void ExampleObject::draw() 
 {
+    glBegin(GL_TRIANGLES);
+    glVertex3d(5,3,0);
+    glVertex3d(2,4,0);
+    glVertex3d(-1,3,0);
+    glEnd();
+    
     glBegin(GL_QUADS);
     glVertex3d(0,0,0);
     glVertex3d(4,0,0);
