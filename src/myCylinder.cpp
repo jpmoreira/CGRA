e5d2ff3d833c1c1e8c716f478642acd972a10d81
Jose Moreira <ei12002@fe.up.pt>
2014-03-26 21:42:43 +0000

    merge issue fixed

--- src/myCylinder.cpp
+++ src/myCylinder.cpp
@@ -38,37 +38,74 @@ void myCylinder::draw(int opt){
 
 void myCylinder::drawRing(int stackNr){
 
-    //printf("drawing\n");
-    CylindricalPoint cylPt1,cylPt2,cylNormal;
-    cylPt1.r=0.5;
-    cylPt1.y=stackNr/(double)stacks-0.5;
-    cylPt2.r=0.5;
-    cylPt2.y=cylPt1.y+1/(double)stacks;
-    cylNormal.y=0;
-    cylNormal.r=1.0;
-    CartesianPoint cartPt1,cartPt2,cartNormal;
-    
-    
+	//printf("drawing\n");
+	float stackHeight=1.0/stacks;
+	CylindricalPoint cylPt1,cylPt2,cylNormal;
+	cylPt1.r=0.5;
+	cylPt1.y=stackNr/(double)stacks-0.5;
+	cylPt2.r=0.5;
+	cylPt2.y=cylPt1.y+1/(double)stacks;
+	cylNormal.y=0;
+	cylNormal.r=1.0;
+	CartesianPoint cartPt1,cartPt2,cartNormal;
 
-    
-    for (int i=0; i<slices; i++) {
-        
-        cylPt1.rho=360./slices*(i-1/2.0);
-        
-        cartPt1=cylindricalToCartesian(&cylPt1);
-        cylPt2.rho=360./slices*(i+1/2.0);
-        cartPt2=cylindricalToCartesian(&cylPt2);
-        cylNormal.rho=360./slices*i;
-        cartNormal=cylindricalToCartesian(&cylNormal);
-        
-        glNormal3d(cartNormal.x, cartNormal.y, cartNormal.z);
-        glVertex3d(cartPt1.x, cartPt1.y, cartPt1.z);
-        glVertex3d(cartPt2.x, cartPt1.y, cartPt2.z);
-        glVertex3d(cartPt2.x, cartPt2.y, cartPt2.z);
-        glVertex3d(cartPt1.x, cartPt2.y, cartPt1.z);
-        
-        
-        
-    }
+
+	//printf("Y= %f stackNr= %d conta= %f\n",cylPt1.y,stackNr,stackNr/(double)stacks);
+
+	for (int i=0; i<slices; i++) {
+
+		cylPt1.rho=360./slices*(i-1/2.0);
+
+		cartPt1=cylindricalToCartesian(&cylPt1);
+		cylPt2.rho=360./slices*(i+1/2.0);
+		cartPt2=cylindricalToCartesian(&cylPt2);
+		cylNormal.rho=360./slices*i;
+		cartNormal=cylindricalToCartesian(&cylNormal);
+
+		glNormal3d(cartNormal.x, cartNormal.y, cartNormal.z);
+		glVertex3d(cartPt1.x, cartPt1.y, cartPt1.z);
+		glVertex3d(cartPt2.x, cartPt1.y, cartPt2.z);
+		glVertex3d(cartPt2.x, cartPt2.y, cartPt2.z);
+		glVertex3d(cartPt1.x, cartPt2.y, cartPt1.z);
+
+
+
+	}
+
+}
+void myCylinder::drawRingVertex(int stackNr){
+
+	float stackHeight=1.0/stacks;
+	CylindricalPoint cylPt1,cylPt2;
+	cylPt1.r=0.5;
+	cylPt1.y=stackNr/(double)stacks-0.5;
+	cylPt2.r=0.5;
+	cylPt2.y=cylPt1.y+1/(double)stacks;
+	CartesianPoint cartPt1,cartPt2 ;
+
+
+	for (int i=0; i<slices; i++) {
+
+		cylPt1.rho=360./slices*(i-1/2.0);
+
+		cartPt1=cylindricalToCartesian(&cylPt1);
+		cylPt2.rho=360./slices*(i+1/2.0);
+		cartPt2=cylindricalToCartesian(&cylPt2);
+
+		glNormal3d(cartPt1.x, cartPt1.y, cartPt1.z);
+		glVertex3d(cartPt1.x, cartPt1.y, cartPt1.z);
+
+		glNormal3d(cartPt2.x, cartPt1.y, cartPt2.z);
+		glVertex3d(cartPt2.x, cartPt1.y, cartPt2.z);
+
+		glNormal3d(cartPt2.x, cartPt2.y, cartPt2.z);
+		glVertex3d(cartPt2.x, cartPt2.y, cartPt2.z);
+
+		glNormal3d(cartPt1.x, cartPt2.y, cartPt1.z);
+		glVertex3d(cartPt1.x, cartPt2.y, cartPt1.z);
+
+
+
+	}
 
 }
